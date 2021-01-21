1b5b9d6854b36cacf303a9d0ff0fd97450375375
caleeq <caleeq@126.com>
2021-01-22 02:57:33 +0800

    format

--- test.js
+++ test.js
@@ -1,27 +1,27 @@
-var data=[
+var data = [
     ["ABCDEFGHI"],
     ["ABCDE"],
     ["A"],
     ["ABCDEFG"]
 ];
 for (var row = 0; row < 4; row++) {
-    var num=0;
+    var num = 0;
     for (var col = 0; col < data[row][0].length; col++) {
         var byte = data[row][0].charAt(col);
         switch (byte) {
             case 'A':
-                num=num+4;
+                num = num + 4;
                 break;
             case 'B':
-                num=num+2;
+                num = num + 2;
                 break;
             case 'C':
-                num=num+2;
+                num = num + 2;
                 break;
             default:
-                num=num+1
+                num = num + 1
                 break;
         }
     }
-    console.log(num);   
+    console.log(num);
 }
