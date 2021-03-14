67569403038ed0edfc4305dc35245ba1681b565f
caleeq <caleeq@126.com>
2021-03-14 21:00:36 +0800

    3/14

--- test.js
+++ test.js
@@ -25,3 +25,36 @@ for (var row = 0; row < 4; row++) {
     }
     console.log(num);
 }
+
+function generateTable(dataArray) {
+    $("#content").empty();
+    var tbody = document.getElementById("content");
+    dataArray.forEach(function (r, index) {
+        var row = document.createElement("tr");
+        for (var i = 0; i < r.length + 1; i++) {
+            var col = document.createElement("td");
+            col.textContent = r[i - 1];
+            row.appendChild(col);
+        }
+        tbody.appendChild(row);
+    });
+}
+
+
+function ajax() {
+    $.post(ctx + "/book/findBook", {"queryBookName": $("#test").val()}, function (data) {
+        $("#list").empty();
+        if ($("#test").val() !== "") {
+            const book = JSON.parse(data);
+            for (let i = 0; i < book.length; i++) {
+                const row = document.createElement("tr");
+                for (const name in book[i]) {
+                    const col = document.createElement("td");
+                    col.textContent = book[i][name];
+                    row.append(col);
+                }
+                $("#list").append(row);
+            }
+        }
+    })
+}
